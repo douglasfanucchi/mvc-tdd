24967cadbe6f829cec366b13686a2e7158b46b0a
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-28 21:14:49 -0300

    parse json body test written

--- tests/core/KernelTest.php
+++ tests/core/KernelTest.php
@@ -5,5 +5,21 @@ namespace Core;
 use PHPUnit\Framework\TestCase;
 
 class KernelTest extends TestCase {
-    
+    /**
+     * @dataprovider jsonProvider
+     */
+    public function testRequestBodyParseJSON(string $json) : void {
+        $body_parsed = (new Kernel)->bodyParseJSON($json);
+        $this->assertEquals(json_encode($body_parsed), $json);
+    }
+
+    private function jsonProvider() : string{
+        $stdObject = new stdClass;
+
+        $stdObject->name  = 'Douglas';
+        $stdObject->email = 'douglasfanucchi@gmail.com';
+        $stdObject->pass  = 'test123';
+
+        return json_encode( $stdObject );
+    }
 }
